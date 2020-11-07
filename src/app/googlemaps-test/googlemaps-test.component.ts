c843c09aada12b6d438c3a54740ae71ba9c7a61c
John Webber <jlweb58@gmail.com>
2020-11-07 08:43:22 +0100

    Initial google maps test commit

---
+++ src/app/googlemaps-test/googlemaps-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-googlemaps-test',
+  templateUrl: './googlemaps-test.component.html',
+  styleUrls: ['./googlemaps-test.component.css']
+})
+export class GooglemapsTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
